2810445afd772f51177e8b3c1f7b7590680fbda9
andrey_mazo <andreymazo@mail.ru>
2023-09-07 19:36:56 +0300

    no comments

--- tbot/management/commands/2.py
+++ tbot/management/commands/2.py
@@ -20,14 +20,10 @@ def start(message):
         with open("polzov_soglash.txt", "rb") as f: 
             contents = f.read().decode("UTF-8") 
             bot.send_message(message.chat.id, contents, reply_markup=markup)
-        
-        # bot.send_message(message.from_user.id, "Как тебя зовут?")
-        # bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
     else:
         bot.send_message(message.from_user.id, 'Напиши /reg')
 
 def get_name(message): #получаем фамилию
-    print('222222222222222222222222222')
     global name
     name = message.text
     bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
@@ -49,8 +45,7 @@ def get_tlf(message):
     #          bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
     tlf = message.text
     keyboard = types.InlineKeyboardMarkup() #наша клавиатура
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes') #кнопка «Да»
-    # keyboard.add(key_yes) #добавляем кнопку в клавиатуру
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
     key_no= types.InlineKeyboardButton(text='Нет', callback_data='no')
     keyboard.add(key_yes,key_no)
     question = 'Ваш телефон '+str(tlf)+' вас зовут '+name+' '+surname+'?'
@@ -62,24 +57,23 @@ def get_tlf(message):
 def callback_worker(call):
     if call.data == 'accept':
         mes = bot.send_message(call.message.chat.id, "Как тебя зовут?")
-        bot.register_next_step_handler(mes, get_name) #следующий шаг – функция get_name
-        print('111111111111111111111')
+        bot.register_next_step_handler(mes, get_name) 
 
-    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        # print('call.message.from_user.id', call.message.from_user.id)
+    if call.data == "yes": 
+       
         username_lst = User.objects.all().values_list('username', flat=True)
         print('username_lst', username_lst)
         print('name, surname', name, surname)
         if not str(name + surname + tlf) in username_lst:
             
             j = User.objects.all().create(username=str(name + surname + tlf))
-            j.save() #код сохранения данных, или их обработки
+            j.save() 
             bot.send_message(call.message.chat.id, 'Запомню : )')
         else:
             bot.send_message(call.message.chat.id, 'Уже есть у нас в базе : )')
             
     elif call.data == "no":
-        bot.send_message(call.message.chat.id, 'Напиши /reg') #переспрашиваем
+        bot.send_message(call.message.chat.id, 'Напиши /reg')
 
 bot.polling(non_stop=True)
 
