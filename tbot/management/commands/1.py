f3e97ebe38ac3b19b601b178f0c7ca248614b2fc
andrey_mazo <andreymazo@mail.ru>
2023-09-07 17:47:40 +0300

    command file 2

--- tbot/management/commands/1.py
+++ tbot/management/commands/1.py
@@ -9,67 +9,107 @@ get_user_model()
 bot = telebot.TeleBot(settings.Token)
 
 # @bot.message_handler(commands=['start', 'button'])
-@bot.message_handler(commands=['start'])
+# @bot.message_handler(commands=['start'])
+@bot.message_handler(content_types=['text'])
 def start(message):
-    if message.text == '/start':
+    if message.text.lower()  == 'start':
         
         markup = types.InlineKeyboardMarkup(row_width=1) 
         item1 = types.InlineKeyboardButton('Посмотреть пользовательское соглашение', callback_data='first')
-        item2 = types.InlineKeyboardButton('Принять условия пользовательского соглашения', callback_data='second')
-        item3 = types.InlineKeyboardButton('Отклонить условия пользовательского соглашения', callback_data='third')
-        markup.add(item1, item2, item3)
+        # item2 = types.InlineKeyboardButton('Принять условия пользовательского соглашения', callback_data='second')
+        # item3 = types.InlineKeyboardButton('Отклонить условия пользовательского соглашения', callback_data='third')
+        markup.add(item1)
         # url = 'https://'
         # markup.add(types.InlineKeyboardButton('Открыть пользовательское соглашение', url=url))
-        bot.send_message(message.chat.id, '<b>Privet</b>', parse_mode='html', reply_markup=markup)
-        # with open("polzov_soglash.txt", "rb") as f: 
-        #   contents = f.read().decode("UTF-8") 
-        #   bot.reply_to(message, contents)
+        bot.send_message(message.chat.id, '<b>Privet</b>', parse_mode='html', reply_markup=markup)#
     else:
-        bot.send_message(message.chat.id, 'Я вас не понимаю, надо набрать /start')
+        bot.send_message(message.chat.id, 'Я вас не понимаю, надо набрать start')
+##################################
+# bot.register_next_step_handler
+# https://www.youtube.com/watch?v=yFdzGEAYiiE
+###################################
+# @bot.message_handler(commands=['phonenumber'])
 
-@bot.message_handler(commands=['phonenumber'])
-def phone(message):
-        print('5555555555555555')
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        button_phone = types.KeyboardButton(text="Отправить телефон",
-                                            request_contact=True)
-        keyboard.add(button_phone)
-        bot.send_message(message.chat.id, 'Номер телефона',
-                         reply_markup=keyboard)
+######################################## Tak zanosit srazu i prosto potom vidergivat ################
+# @bot.message_handler(content_types=['contact'])
+# def phone_name(message):
+#         print('5555555555555555')
+#         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+#         button_contact = types.KeyboardButton(text="Отправить данные",
+#                                             request_contact=True)
+        
+#         keyboard.add(button_contact)
+#         if message.contact:
+#             print('here iiiiiiiiii')
+#             bot.send_message(message.chat.id, message.contact.phone_number, reply_markup=keyboard)
+######################################################################################################
+@bot.message_handler(content_types=['text'])
+def phone_name(message):
+    # https://telegra.ph/Pishem-svoego-telegram-bota-na-Python-ch2-12-13
+    # https://ru.stackoverflow.com/questions/705633/%D0%9A%D0%B0%D0%BA-%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BE%D1%82-usera-%D0%BA%D0%BE%D1%82%D0%BE%D1%80%D0%BE%D0%B5-%D0%BE%D0%BD-%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB-%D0%B2-%D0%BE%D1%82%D0%B2%D0%B5%D1%82-%D0%BD%D0%B0-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B1%D0%BE%D1%82%D0%B0?ysclid=lm8a86wq43972552168
 
-@bot.message_handler(content_types=['contact'])
-def contact(message):
-    print('6666666666666666', message.contact.phone_number)
-    if message.contact is not None:
-        keyboard2 = types.ReplyKeyboardRemove()
-        bot.send_message(message.chat.id, 'Вы успешно отправили свой номер', reply_markup=keyboard2)
-        global phonenumber
-        phonenumber= str(message.contact.phone_number)
-        user_id = str(message.contact.user_id)
-        print('7777777777777777777777777777777777777', phonenumber, user_id)
+    # mm = types.ReplyKeyboardMarkup(row_width=2)
+    # keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    button_name = types.InlineKeyboardButton("😎 Введите имя", callback_data='name')
+    button_phone = types.InlineKeyboardButton("😥 Введите телефон", callback_data='phone')
+    # button_name = types.KeyboardButton(text="Отправить имя")
+    # button_phone = types.KeyboardButton(text="Отправить телефон")
+    # button1 = types.KeyboardButton("🐣 Привет")
+    # button2 = types.KeyboardButton("😀 Как дела?")
+    keyboard.add(button_name, button_phone)
+    bot.send_message(message.chat.id, f'{message.text}', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['command'])
+def _command_(message):
+     bot.send_message(message.chat.id, "Введите имя: ")
+     bot.register_next_step_handler(message, add_user)
+
+def add_user(message):
+    #тут функция записи в бд
+    j = User.objects.create(id = f'{message.from_user.id}', first_name=f'{message.text}')
+    j.save()
+    
+# https://ru.stackoverflow.com/questions/1280092/%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%BE%D0%BC%D0%B5%D1%80%D0%B0-%D1%82%D0%B5%D0%BB%D0%B5%D1%84%D0%BE%D0%BD%D0%B0-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5-%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%BA%D0%B8-%D0%B5%D0%B3%D0%BE-%D0%B2-telegram?ysclid=lm7qppa83o668821030
+# @bot.message_handler(content_types=['contact'])
+# def contact(message):
+#     print('6666666666666666', message.contact.phone_number)
+#     if message.contact is not None:
+#         keyboard2 = types.ReplyKeyboardRemove()
+#         bot.send_message(message.chat.id, 'Вы успешно отправили свой номер', reply_markup=keyboard2)
+#         global phonenumber
+#         phonenumber= str(message.contact.phone_number)
+#         user_id = str(message.contact.user_id)
+#         print('7777777777777777777777777777777777777', phonenumber, user_id)
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback(call):
     if call.message:
         if call.data == 'first':
+            markup = types.InlineKeyboardMarkup(row_width=1)#, resize_keyboard=True) 
+            item2 = types.InlineKeyboardButton('Принять условия пользовательского соглашения', callback_data='second')
+            item3 = types.InlineKeyboardButton('Отклонить условия пользовательского соглашения', callback_data='third')
+            markup.add(item2, item3)
+           
             # markup = types.InlineKeyboardMarkup(row_width=1) 
             # item2 = types.InlineKeyboardButton('Принять условия пользовательского соглашения', callback_data='second')
             # item3 = types.InlineKeyboardButton('Отклонить условия пользовательского соглашения', callback_data='third')
             # markup.add(item2, item3)
             with open("polzov_soglash.txt", "rb") as f: 
                 contents = f.read().decode("UTF-8") 
-            bot.send_message(call.message.chat.id, contents)
+            bot.send_message(call.message.chat.id, contents, reply_markup=markup)
             
 
         elif call.data == 'second':
             # Proverka vvoda s telefonom i proch
-            bot.send_message(call.message.chat.id, f'Введите ваш номер телефона')
-            phone(call.message)
-            contact(call.message)
-            if call.message.text==phonenumber:
-                  bot.send_message(call.message.chat.id, 'Порядок')
-            else:
-                bot.send_message(call.message.chat.id, f"Непорядок, ваш номер {phonenumber}")
+            # bot.send_message(call.message.chat.id, f'Введите ваши данные')
+            phone_name(call.message)
+            # contact(call.message)
+            # if call.message.text==phonenumber:
+            #       bot.send_message(call.message.chat.id, 'Порядок')
+            # else:
+            #     bot.send_message(call.message.chat.id, f"Непорядок, ваш номер {phonenumber}")
                 # j = User.objects.create(username=f'{call.message.from_user.id}')
                 # j.save()
         elif call.data == 'third':
@@ -79,6 +119,11 @@ def callback(call):
             #     bot.reply_to(message, contents)
             #     j = User.objects.create(username=f'{message.from_user.id}')
             #     j.save()
+        elif call.data == 'name':
+            print(f'name {call.message.text}')
+        elif call.data == 'phone':
+            print(f'phone {call.message.text}')
+
 # @bot.message_handler(content_types=["text"])
 # def answer(message):
 #     if message.text == message.contact.phone_number:
---
+++ tbot/management/commands/2.py
@@ -0,0 +1,92 @@
+from django.core.management import BaseCommand
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User 
+import telebot 
+from telebot import types
+from config import settings
+
+name = ''
+surname = ''
+tlf = 0
+bot = telebot.TeleBot(settings.Token)
+get_user_model()
+
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        markup = types.InlineKeyboardMarkup(row_width=1)#, resize_keyboard=True) 
+        item = types.InlineKeyboardButton('Принять условия пользовательского соглашения', callback_data='accept')
+        markup.add(item)
+        with open("polzov_soglash.txt", "rb") as f: 
+            contents = f.read().decode("UTF-8") 
+            bot.send_message(message.chat.id, contents, reply_markup=markup)
+        
+        # bot.send_message(message.from_user.id, "Как тебя зовут?")
+        # bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg')
+
+def get_name(message): #получаем фамилию
+    print('222222222222222222222222222')
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
+    print('name', name)
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, 'Номер вашего телефона?')
+    bot.register_next_step_handler(message, get_tlf)
+
+def get_tlf(message):
+    global tlf
+    # while tlf == 0: #проверяем что возраст изменился
+    #     try:
+    #          tlf = int(message.text) #проверяем, что  введен корректно
+    #     except Exception:
+    #          bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+    tlf = message.text
+    keyboard = types.InlineKeyboardMarkup() #наша клавиатура
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes') #кнопка «Да»
+    # keyboard.add(key_yes) #добавляем кнопку в клавиатуру
+    key_no= types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_yes,key_no)
+    question = 'Ваш телефон '+str(tlf)+' вас зовут '+name+' '+surname+'?'
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+#  этот хендлер сработает только на тех, у которых callback_data начинается с bike_model
+# @bot.callback_query_handler(func=lambda call: call.data.startswith('bike_model'))
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == 'accept':
+        mes = bot.send_message(call.message.chat.id, "Как тебя зовут?")
+        bot.register_next_step_handler(mes, get_name) #следующий шаг – функция get_name
+        print('111111111111111111111')
+
+    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
+        # print('call.message.from_user.id', call.message.from_user.id)
+        username_lst = User.objects.all().values_list('username', flat=True)
+        print('username_lst', username_lst)
+        print('name, surname', name, surname)
+        if not str(name + surname + tlf) in username_lst:
+            
+            j = User.objects.all().create(username=str(name + surname + tlf))
+            j.save() #код сохранения данных, или их обработки
+            bot.send_message(call.message.chat.id, 'Запомню : )')
+        else:
+            bot.send_message(call.message.chat.id, 'Уже есть у нас в базе : )')
+            
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, 'Напиши /reg') #переспрашиваем
+
+bot.polling(non_stop=True)
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        
+        from django.db import connection
+        db_name = connection.settings_dict['NAME']
+        print(db_name)
